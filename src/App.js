933015056b85e52e2d8bc62f23e72a312b4f5ab5
ananari <anaismahmed@uchicago.edu>
2021-01-24 17:03:07 -0600

    troubleshooting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Sidebar from './Sidebar.js';
-import {HashRouter, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Home from './Home.js';
 import SylotiApp from './SylotiKeyboard.js';
 
@@ -7,16 +7,16 @@ function App() {
   return (
     <div className="App">
       <Sidebar/>
-      <HashRouter basename={process.env.PUBLIC_URL}>
-      <Switch>
-        <Route exact path={process.env.PUBLIC_URL + '/'}>
-          <Home className="content"/>
-        </Route>
-        <Route exact path={process.env.PUBLIC_URL + "/sylotikeyboard"}>
-          <SylotiApp />
-        </Route>
-      </Switch>
-      </HashRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Switch>
+          <Route exact path={process.env.PUBLIC_URL + '/'}>
+            <Home className="content"/>
+          </Route>
+          <Route exact path={process.env.PUBLIC_URL + "/sylotikeyboard"}>
+            <SylotiApp />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
